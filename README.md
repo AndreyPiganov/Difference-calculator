417a50b0f10cb41ad40c82e83fd2e574baa6a21c
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-16 12:15:13 +0300

    Recreating a project for review by a mentor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AndreyPiganov/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/AndreyPiganov/frontend-project-46/actions)
+[![Node CI](https://github.com/AndreyPiganov/frontend-project-46/actions/workflows/node.js.yml/badge.svg)](https://github.com/AndreyPiganov/frontend-project-46/actions/workflows/node.js.yml)
+
+Example:
+
+[![asciicast](https://asciinema.org/a/ep4rx1eTperAFmzetYk0DFx55.svg)](https://asciinema.org/a/ep4rx1eTperAFmzetYk0DFx55)
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import parse from './parsers.js';
+import { getFormat, readFile } from './utils.js';
+import buildTree from './buildTree.js';
+import formatter from './formatter/index.js';
+
+// eslint-disable-next-line no-unused-vars
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
+  const file1 = readFile(filePath1);
+  const file2 = readFile(filePath2);
+
+  const obj1 = parse(file1, getFormat(filePath1));
+  const obj2 = parse(file2, getFormat(filePath2));
+
+  const tree = buildTree(obj1, obj2);
+  return formatter(tree, format);
+};
+export default genDiff;
