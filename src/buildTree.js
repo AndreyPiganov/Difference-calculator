9ede17e0a5a13e0fa801bbb71871a378b744758a
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-07 18:02:16 +0300

    fix step 4

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,26 +1,24 @@
 import _ from 'lodash';
 
 const buildTree = (object1, object2) => {
-  const clone1 = _.cloneDeep(object1);
-  const clone2 = _.cloneDeep(object2);
 
-  const keys = _.sortBy(_.union(_.keys(clone1), _.keys(clone2)));
+  const keys = _.sortBy(_.union(_.keys(object1), _.keys(object2)));
 
   const tree = keys.map((key) => {
-    if (!_.has(clone2, key)) {
-      return { name: key, status: 'deleted', value: clone1[key] };
+    if (!_.has(object2, key)) {
+      return { name: key, status: 'deleted', value: object1[key] };
     }
-    if (!_.has(clone1, key)) {
-      return { name: key, status: 'added', value: clone2[key] };
+    if (!_.has(object1, key)) {
+      return { name: key, status: 'added', value: object2[key] };
     }
-    if (_.isObject(clone1[key]) && _.isObject(clone2[key])) {
-      return { name: key, status: 'nested', children: buildTree(clone1[key], clone2[key]) };
+    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+      return { name: key, status: 'nested', children: buildTree(object1[key], object2[key]) };
     }
-    if (_.isEqual(clone1[key], clone2[key]) === true) {
-      return { name: key, status: 'unchanged', value: clone1[key] };
+    if (_.isEqual(object1[key], object2[key]) === true) {
+      return { name: key, status: 'unchanged', value: object1[key] };
     }
     return {
-      name: key, status: 'changed', value1: clone1[key], value2: clone2[key],
+      name: key, status: 'changed', value1: object1[key], value2: object2[key],
     };
   });
   return tree;
