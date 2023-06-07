0ba4eba088007ddb9b8ed1e005e754a75582b810
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-07 17:59:10 +0300

    eslint fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { getFormat, readFile } from './utils.js';
 import buildTree from './buildTree.js';
 import formatter from './formatter/index.js';
 
-// eslint-disable-next-line no-unused-vars
 const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const file1 = readFile(filePath1);
   const file2 = readFile(filePath2);
